e383f680324bbd8f793e868073f3bb5265dc24ea
Jayden Szekely <jaydenszekely@gmail.com>
2019-06-04 13:40:54 +1200

    Grey

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,8 +3,8 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-home-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/home.js" /* webpackChunkName: "component---src-templates-home-js" */),
-  "component---src-templates-about-page-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/about-page.js" /* webpackChunkName: "component---src-templates-about-page-js" */),
   "component---src-templates-blog-post-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
+  "component---src-templates-about-page-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/about-page.js" /* webpackChunkName: "component---src-templates-about-page-js" */),
   "component---src-templates-all-articles-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/all-articles.js" /* webpackChunkName: "component---src-templates-all-articles-js" */),
   "component---src-templates-resource-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/resource.js" /* webpackChunkName: "component---src-templates-resource-js" */),
   "component---src-templates-resource-page-js": () => import("/Users/jaydenszekely/Development/jaydenszekely/src/templates/resource-page.js" /* webpackChunkName: "component---src-templates-resource-page-js" */),
