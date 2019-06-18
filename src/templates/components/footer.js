2fe1fb9a797b5f37cc319d48638c93b27b5c54ce
Jayden Szekely <jaydenszekely@gmail.com>
2019-06-18 16:12:59 +1200

    Hello

--- src/templates/components/footer.js
+++ src/templates/components/footer.js
@@ -9,7 +9,7 @@ const Footer = ({children, background, recommendedTitle, authorName, authorBio})
                 <br />
                 <p>
                     ph – <a href="tel: +64211222857" title="Call Jayden Szekely">(+64) 021 122 2857</a><br />
-                    em – <a href="mailto: jaydenszekely@icloud.com" title="Email Jayden Szekely">jaydenszekely@icloud.com</a><br />
+                    em – <a href="mailto: hello@jaydenszekely.com" title="Email Jayden Szekely">hello@jaydenszekely.com</a><br />
                     ig – <a href="https://www.instagram.com/jaydenszekely" title="Jayden Szekely on Instagram">@jaydenszekely</a>
                     <br /><br /><br />
                     © Jayden Szekely, 2019
